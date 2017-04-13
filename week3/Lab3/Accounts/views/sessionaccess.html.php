521caf7e613ce62cda5e5bb75f2243614243f1dd
Overlord_Zoltron <jmarsenault@email.neit.edu>
2017-04-13 12:21:28 -0400

    Fixed access to the admin page to display 'not allowed' message in the center, and included a link for users to go back to login page

--- week3/Lab3/Accounts/views/sessionaccess.html.php
+++ week3/Lab3/Accounts/views/sessionaccess.html.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
 if (!isset($_SESSION['user_id']) || $_SESSION['user_id'] <= 0) {
-    exit('You are not allowed, please login');
+    exit('<h1 class="text-center">You are not allowed, please <a href="./Login.php">login</a></h1>');
 }
